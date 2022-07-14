6ae91e6c5e97fc1ec965bc84e54fa74f9fdccceb
cumabay <marlen_qrm@yahoo.com>
2022-07-14 08:56:23 +0300

    fix

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,100 +1,92 @@
-import React, { useState } from 'react'
-import data from '../mock-data.json'
-import { nanoid } from 'nanoid'
+import React, { useState } from 'react';
+import data from '../mock-data.json';
+import { nanoid } from 'nanoid';
+import axioaApi from '../api/axios/axios-baseurl'
 
 function Table() {
-  const [contacts, setContacts] = useState(data)
-  const [addFormData, setAddFormData] = useState({
-    fullName: '',
-    address: '',
-    phoneNumber: '',
-    truckNum: ''
+  const dataTest = axioaApi.get('trucks/')
+  .then(res => {
+    const tracks = res.data;
+    this.setState ({ tracks })
   })
+
+
+  const [contacts, setContacts] = useState(data);
+  const [addFormData, setAddFormData] = useState({ fullName: ', address: ', phoneNumber: ', truckNum: ' });
 
   // eslint-disable-next-line no-extend-native
   String.prototype.capitalizeFirstLetter = function () {
     return this.charAt(0).toUpperCase() + this.slice(1);
-  }
+  };
 
-  const handleAddFormChance = (event) => {
-    event.preventDefault()
+  const handleAddFormChance = event => {
+    event.preventDefault();
 
-    const fieldName = event.target.getAttribute('name')
-    const fieldValue = event.target.value.capitalizeFirstLetter()
+    const fieldName = event.target.getAttribute('name');
+    const fieldValue = event.target.value.capitalizeFirstLetter();
 
-    const newFormData = { ...addFormData }
-    newFormData[fieldName] = fieldValue
+    const newFormData = {
+      ...addFormData
+    };
+    newFormData[fieldName] = fieldValue;
 
-    setAddFormData(newFormData)
-  }
+    setAddFormData(newFormData);
+  };
 
-
-  const handleAddFormSubmit = (event) => {
-    event.preventDefault()
+  const handleAddFormSubmit = event => {
+    event.preventDefault();
 
     const newContact = {
       id: nanoid(),
       fullName: addFormData.fullName,
       address: addFormData.address,
       phoneNumber: addFormData.phoneNumber,
-      truckNum: addFormData.truckNum,
-    }
+      truckNum: addFormData.truckNum
+    };
 
-    const newContacts = [...contacts, newContact]
-    setContacts(newContacts)
-  }
+    const newContacts = [
+      ...contacts,
+      newContact
+    ];
+    setContacts(newContacts);
+  };
 
-  return (<div className="conteiner content">
+  return (<div className='conteiner content'>
     <h4>Добавить Заезд</h4>
     <form onSubmit={handleAddFormSubmit}>
-      <input type="text"
-        name="fullName"
-        required="required"
-        placeholder="Фамилия Имя"
-        onChange={handleAddFormChance}
-      />
-      <input type="text"
-        name="address"
-        required="required"
-        placeholder="Направление"
-        onChange={handleAddFormChance}
-      />
-      <input type="text"
-        name="phoneNumber"
-        required="required"
-        placeholder="Телефон:"
-        onChange={handleAddFormChance}
-      />
-      <input type="text"
-        name="truckNum"
-        required="required"
-        placeholder="Номер машины"
-        onChange={handleAddFormChance}
-      />
-      <button className="btn boxBtn" type="submit">Добавить</button>
+      <input type='text' name='fullName' required='required' placeholder='Фамилия Имя' onChange={handleAddFormChance} />
+      <input type='text' name='address' required='required' placeholder='Направление' onChange={handleAddFormChance} />
+      <input type='text' name='phoneNumber' required='required' placeholder='Телефон:' onChange={handleAddFormChance} />
+      <input type='text' name='truckNum' required='required' placeholder='Номер машины' onChange={handleAddFormChance} />
+      <button className='btn boxBtn' type='submit'>
+        Добавить
+      </button>
     </form>
-    <table className="centered">
-      <thead className="card-panel green lighten-5">
+    <table className='centered'>
+      <thead className='card-panel green lighten-5'>
         <tr>
+          <th>Въезд/Выезд</th>
           <th>Имя Фамилия</th>
-          <th>Адрес Отправки</th>
-          <th>Номер Телефона</th>
-          <th>Номер Машины</th>
+          <th>Телефон</th>
+          <th>Номер авто</th>
+          <th>Цель въезда</th>
+          <th>Номенклатура</th>          
         </tr>
       </thead>
       <tbody>
-        {contacts.map((contact) => (
-          <tr>
+        {
+          contacts.map(contact => (<tr>
             <td>{contact.fullName}</td>
             <td>{contact.address}</td>
             <td>{contact.phoneNumber}</td>
             <td>{contact.truckNum}</td>
-          </tr>
-        ))}
+            <td></td>
+            <td></td>
+          </tr>))
+        }
       </tbody>
     </table>
-  </div>
-  )
+  </div>);
 }
 
-export default Table
+export default Table;
