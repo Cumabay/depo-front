eae988b1399094f0791922577e92cd39aa5da7f9
Asan <isliamov1@gmail.com>
2022-07-17 20:01:29 +0300

    Login

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import styles from './Login.module.css'
+import Logo from '../icons/circle-user-solid.svg'
+import Password from '../icons/lock-solid.svg'
 
 function Login() {
   return (
-    <div>
-    
-       
+    <div className={styles.wrapper}>
+      <div className={styles.conteiner}>
+        <div className={styles.title}>
+          <div className={styles.boxForm}>
+            <span>Вход</span>
+            <div>
+              <div className={styles.logins}>
+                <form>
+                  <img src={Logo} alt="Login" />
+                  <input type="text" placeholder="Введите логин" required />
+                </form>
+              </div>
+              <div className={styles.logins}>
+                <form>
+                  <img src={Password} alt="Login" />
+                  <input type="text" placeholder="Пароль" required />
+                </form>
+              </div>
+              <div className={styles.logins}>
+                <input className='btn' type="submit" value="login" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
